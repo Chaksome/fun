6169110b469012be989889550b0328c21b686ac0
frothywater <cobalt@white-cat>
2019-11-26 23:38:38 +0800

    Fix problems~

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 import Hello from "../images/Hello.png"
+import "../styles/index.css"
 
 const IndexPage = () => (
   <Layout>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,8 +1,3 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 import seeyou from "../images/seeyou.png"
 
 const SecondPage = () => (
@@ -11,7 +6,7 @@ const SecondPage = () => (
     <h1>Bye~</h1>
     <img src={seeyou} alt="Don't let me see you again " />
     <p>
-    <Link to="/"> Back to the homepage</Link>
+      <Link to="/"> Back to the homepage</Link>
     </p>
   </Layout>
 )
