784f0aeca4bf19945fd951ac9acf21f954c5cbf8
Chaksome <1952084882@qq.com>
2019-12-08 01:49:24 +0800

    Fix Nothing

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import seeyou from "../images/seeyou.png"
 
 const NotFoundPage = () => (
   <Layout>
