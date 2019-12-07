e03c3507925f21c19ef0bc154a6c263a8018d517
Chaksome <1952084882@qq.com>
2019-12-08 01:21:22 +0800

    Nothing

---
+++ src/pages/emotion-sample.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { css } from "@emotion/core"
+export default () => (
+  <div>
+    <p
+      css={{
+        background: "pink",
+        color: "blue",
+      }}
+    >
+      This page is using Emotion.
+    </p>
+  </div>
+)
